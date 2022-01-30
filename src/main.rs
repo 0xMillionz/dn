d51e6eac2b75f5d36e55fa1ea29f1a0ee05b7cb1
0xMillionz <madokakaname99@protonmail.com>
2022-01-30 18:51:43 -0500

    restart

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,7 @@
 use tokio::sync::mpsc;
 use anchor_client;
 
-mod executions;
-
-mod state;
-use state::DnState;
-
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
-
-    // Channel...
-    let (tx, mut rx) = mpsc::channel(200);
-
-
-    // Create state
-    let state: &'static DnState = DnState::new();
-
-    // Create dedicated thread is dumb, comprimise and use tokio...
-    tokio::spawn(executions::scrape::mango::sol_perp(state));
+	
 }
